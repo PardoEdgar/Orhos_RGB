59e5b067b3dbe964c488103032b7732411de11d0
E. Alejandro Pardo-Sarmiento <165843464+PardoEdgar@users.noreply.github.com>
2026-06-29 12:59:28 -0500

    Revise data source and improve analysis visuals
    Updated data source and modified columns for analysis. Enhanced visualizations and added statistical summaries.

--- Scripts/Data_analysis.R
+++ Scripts/Data_analysis.R
@@ -1,20 +1,106 @@
 library("tidyverse")
-path <- "C:/Users/jandr/Downloads/Data_pixels.csv"
+path <- "C:/Users/jandr/Downloads/Canales_Data_pixels_12_colonies_automated.csv"
 data <- read.csv(path)
-View(data)
 data_long <- pivot_longer(
   data,
   names_to = "RGB",
-  cols = c(h, s, v),
+  cols = c(R, G, B),
   values_to = "Value"
-)
-View(data_long)
-colors <- c("darkorange", "yellow", "gray")
-ggplot(data_long, aes(x = data_long$Value, fill = data_long$month)) +
-  geom_histogram() +
-  facet_wrap(~ data_long$Type + data_long$RGB, nrow = 2) +
-  scale_fill_manual(values = colors)
+) |>
+  mutate(
+    month_year = factor(
+      paste(month, year),
+      levels = c(
+        "September 2022",
+        "February 2023",
+        "August 2023",
+        "September 2023",
+        "January 2024"
+      )
+    )
+  )
+stat_summary <- data_long |>
+  group_by(month, year, Type, RGB) |>
+  summarise(
+    mean = mean(Value),
+    sd = sd(Value),
+    SE = sd / sqrt(n()),
+    .groups = "drop"
+  ) |>
+  mutate(
+    month_year = factor(
+      paste(month, year),
+      levels = c(
+        "September 2022",
+        "February 2023",
+        "August 2023",
+        "September 2023",
+        "January 2024"
+      )
+    )
+  )
 
+colors <- c("darkorange", "yellow", "gray", "purple", "darkred")
+
+ggplot(
+  data_long,
+  aes(
+    x = data_long$month_year,
+    y = data_long$Value,
+    color = data_long$month_year
+  )
+) +
+  geom_violin(alpha = 1) +
+  geom_jitter(size = 0.5, alpha = 0.1, width = 0.2) +
+  geom_boxplot(
+    width = 0.2,
+    position = position_identity(),
+    color = "black",
+    alpha = 0.5
+  ) +
+  geom_errorbar(
+    data = stat_summary,
+    aes(x = month_year, ymax = mean + SE, ymin = mean - SE),
+    width = 0.2,
+    inherit.aes = FALSE
+  ) +
+  geom_point(
+    data = stat_summary,
+    aes(x = month_year, y = mean),
+    color = "darkred",
+    size = 2,
+    inherit.aes = FALSE
+  ) +
+  facet_wrap(~ Type + RGB, nrow = 2) +
+  scale_color_manual(values = colors) +
+  theme_classic() +
+  xlab("month year") +
+  ylab("Pixel intensity (0-255)") +
+  labs(title = "12 colonies automated RGB plot", color = "month year") +
+  theme(
+    legend.position = "top",
+    title = element_text(size = 20)
+  )
+
+
+ggplot(
+  data_long,
+  aes(
+    x = Value,
+    color = month_year
+  )
+) +
+  geom_density() +
+  facet_wrap(~ Type + RGB, nrow = 2) +
+  scale_color_manual(values = colors) +
+  theme_classic() +
+  xlab("Month Year") +
+  ylab("Value") +
+  labs(title = "Automated", color = "Month & year") +
+  theme(
+    legend.position = "top",
+    title = element_text(size = 20)
+  )
 data_type_3 <- data_long |> dplyr::filter(Type == "Type 3")
 
 kruskal.test(Value ~ month, data_type_3)
