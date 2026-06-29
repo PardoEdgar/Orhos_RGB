7ae14534859e97970a1318fc934bb3e56fe70993
E. Alejandro Pardo-Sarmiento <165843464+PardoEdgar@users.noreply.github.com>
2026-06-29 13:14:06 -0500

    Refactor data analysis script and enhance plotting
    Updated file paths and modified month levels for analysis. Added new plotting functionality with violin and box plots.

--- Scripts/Data_analysis.R
+++ Scripts/Data_analysis.R
@@ -1,5 +1,5 @@
 library("tidyverse")
-path <- "C:/Users/jandr/Downloads/Canales_Data_pixels_12_colonies_automated.csv"
+path <- "C:/Users/PardoEA/Downloads/Data_pixels.csv"
 data <- read.csv(path)
 data_long <- pivot_longer(
   data,
@@ -11,8 +11,8 @@ data_long <- pivot_longer(
     month_year = factor(
       paste(month, year),
       levels = c(
-        "September 2022",
-        "February 2023",
+        "August 2022",
+        "March 2023",
         "August 2023",
         "September 2023",
         "January 2024"
@@ -31,8 +31,8 @@ stat_summary <- data_long |>
     month_year = factor(
       paste(month, year),
       levels = c(
-        "September 2022",
-        "February 2023",
+        "August 2022",
+        "March 2023",
         "August 2023",
         "September 2023",
         "January 2024"
@@ -61,7 +61,7 @@ ggplot(
   geom_errorbar(
     data = stat_summary,
     aes(x = month_year, ymax = mean + SE, ymin = mean - SE),
-    width = 0.2,
+    width = 0.1,
     inherit.aes = FALSE
   ) +
   geom_point(
@@ -102,5 +102,90 @@ ggplot(
     title = element_text(size = 20)
   )
 data_type_3 <- data_long |> dplyr::filter(Type == "Type 3")
+
+kruskal.test(Value ~ month, data_type_3)
+
+
+########################################## Plot con smooth
+data_long_num <- pivot_longer(
+  data,
+  names_to = "RGB",
+  cols = c(R, G, B),
+  values_to = "Value"
+) |>
+  mutate(
+    month_year = factor(
+      paste(month, year),
+      levels = c(
+        "August 2022",
+        "March 2023",
+        "August 2023",
+        "September 2023",
+        "January 2024"
+      )
+    )
+  ) |>  mutate(month_numeric = case_when(month == "August"~ 6,month == "March"~ 3,month == "January"~ 1,month == "September"~ 7))
+
+stat_summary_num <- data_long_num |>
+  group_by(month, year, Type, RGB) |>
+  summarise(
+    mean = mean(Value),
+    sd = sd(Value),
+    SE = sd / sqrt(n()),
+    .groups = "drop"
+  ) |>
+  mutate(
+    month_year = factor(
+      paste(month, year),
+      levels = c(
+        "August 2022",
+        "March 2023",
+        "August 2023",
+        "September 2023",
+        "January 2024"
+      ) 
+    )
+  ) |>  mutate(month_numeric = case_when(month == "August"~ 6,month == "March"~ 3,month == "January"~ 1,month == "September"~ 7))
+
+ggplot(
+  data_long_num,
+  aes(
+    x = data_long_num$month_year,
+    y = data_long_num$Value,
+    color = data_long_num$month_year
+  )
+) +
+  geom_violin(alpha = 1) +
+  geom_jitter(size = 0.5, alpha = 0.1, width = 0.2) +
+  geom_boxplot(
+    width = 0.2,
+    position = position_identity(),
+    color = "black",
+    alpha = 0.5
+  ) +
+  geom_errorbar(
+    data = stat_summary_num,
+    aes(x = month_year, ymax = mean + SE, ymin = mean - SE),
+    width = 0.1,
+    inherit.aes = FALSE
+  ) +
+  geom_point(
+    data = stat_summary_num,
+    aes(x = month_year, y = mean),
+    color = "darkred",
+    size = 2,
+    inherit.aes = FALSE
+  ) +
+  facet_wrap(~ Type + RGB, nrow = 2) +
+  scale_color_manual(values = colors) +
+  theme_classic() +
+  xlab("month year") +
+  ylab("Pixel intensity (0-255)") +
+  labs(title = "12 colonies automated RGB plot", color = "month year") +
+  theme(
+    legend.position = "top",
+    title = element_text(size = 20)
+  )
+
 
 kruskal.test(Value ~ month, data_type_3)
