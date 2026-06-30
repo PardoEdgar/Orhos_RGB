b2286531e5c90ed850d376db91499b4467da344f
E. Alejandro Pardo-Sarmiento <165843464+PardoEdgar@users.noreply.github.com>
2026-06-30 12:00:21 -0500

    Update Histogram_plot_RGB.R

--- Scripts/Histogram_plot_RGB.R
+++ Scripts/Histogram_plot_RGB.R
@@ -1,16 +1,28 @@
 library(arrow)
 library("tidyverse")
-path <- "C:/Users/PardoEA/Downloads/dataset_pixels"
+path <- "C:/Users/PardoEA/Downloads/RGB_data"
 
 data <- open_dataset(path)
 
 data_long <- data |>
-  select(ID, Date, R,G,B) |> 
+  select(ID, month, year, R,G,B) |> 
   collect() |> sample_n(1000000) |>
   pivot_longer(
     cols = c(R, G, B),
     names_to = "Channel",
     values_to = "Value"
+  ) |>
+  mutate(
+    month_year = factor(
+      paste(month, year),
+      levels = c(
+        "August 2022",
+        "March 2023",
+        "August 2023",
+        "September 2023",
+        "January 2024"
+      )
+    )
   )
 
 data_long$Channel <- factor(
@@ -18,14 +30,11 @@ data_long$Channel <- factor(
   levels = c("R", "G", "B")
 )
 
-data_long$Date <- factor(
-  data_long$Date,
-  levels = c("February","January", "September", "August")
-)
 
-colors <- c("chocolate", "gray", "black", "yellow")
 
-ggplot(data_long, aes(x = Value, color = Date)) +
+colors <- c("chocolate", "gray", "black", "yellow", "orange")
+
+ggplot(data_long, aes(x = Value, color = month_year)) +
   geom_density() +
   scale_color_manual(values = colors) +
   facet_wrap(~ID + Channel, ncol = 3) +
